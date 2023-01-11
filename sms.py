b33fc772ef0107d6b13816c236f88cec825104cf
unknown <viczik16@gmail.com>
2023-01-11 19:14:14 +0100

    edited env

--- sms.py
+++ sms.py
@@ -7,26 +7,13 @@ def get_token():
     with open("env.txt", "r") as f:
         return f.read()
 
-def send_sms(number, mess):
-    num = number
-    account_sid = "ACa59320f2dad74cafe284fd22f01f7476"
-    auth_tok = "c9c11862eabd0d08cdeb21f145aa7359"
-    #auth_token= auth_tok[14:-1]
-    
-    client = Client(account_sid, auth_tok)
 
-    message = client.messages.create(
-    body= mess,
-    from_='[+][1][9388882655]',
-    to='[+][234][%s]' %num
-    )
-    #print(message.sid)
     
 
 def sending_sms(number,  mess):
     api_url = 'https://account.kudisms.net/api/'
-    username = 'cseantest1@gmail.com'
-    password = 'Diamond@2007'
+    username = ''
+    password = ''
     sender = 'KudiBrave'
     mobile = number
 
